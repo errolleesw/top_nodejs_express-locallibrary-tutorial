ecf9056cd1a56026e24febee992f2fc81c2cc30f
Errol Lee <62831245+errolleesw@users.noreply.github.com>
2024-07-07 15:48:10 +0800

    Tutorial Part 6
    Completed guide and first challenge to delete a bookinstance.

--- express-locallibrary-tutorial/models/author.js
+++ express-locallibrary-tutorial/models/author.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const { DateTime } = require("luxon");
 
 const Schema = mongoose.Schema;
 
@@ -26,6 +27,18 @@ AuthorSchema.virtual("url").get(function () {
   // We don't use an arrow function as we'll need the this object
   return `/catalog/author/${this._id}`;
 });
+
+// Virtual for formatted author birth date
+AuthorSchema.virtual("date_of_birth_formatted").get(function () {
+  return this.date_of_birth ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED) : '';
+});
+
+// Virtual for formatted author death date
+AuthorSchema.virtual("date_of_death_formatted").get(function () {
+  return this.date_of_death ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED) : '';
+});
+
+
 
 // Export model
 module.exports = mongoose.model("Author", AuthorSchema);
