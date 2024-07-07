ed33e37a770255c7c79346ae89e0d9bf1e177315
Errol Lee <62831245+errolleesw@users.noreply.github.com>
2024-07-07 15:59:28 +0800

    Delete Genre
    Update to include ability to delete genre

--- express-locallibrary-tutorial/controllers/genreController.js
+++ express-locallibrary-tutorial/controllers/genreController.js
@@ -85,12 +85,45 @@ exports.genre_create_post = [
 
 // Display Genre delete form on GET.
 exports.genre_delete_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Genre delete GET");
+  // Get details of all books related to the genre
+  const [genre, booksInGenre] = await Promise.all([
+    Genre.findById(req.params.id).exec(),
+    Book.find({ genre: req.params.id }, "title summary").exec(),
+  ]);
+
+  if (genre === null) {
+    // No results.
+    res.redirect("/catalog/genres");
+  }
+
+  res.render("genre_delete", {
+    title: "Delete Genre",
+    genre: genre,
+    genre_books: booksInGenre,
+  })
 });
 
 // Handle Genre delete on POST.
 exports.genre_delete_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Genre delete POST");
+  // Get details of all books related to the genre
+  const [genre, booksInGenre] = await Promise.all([
+    Genre.findById(req.params.id).exec(),
+    Book.find({ genre: req.params.id }, "title summary").exec(),
+  ]);
+
+  if (booksInGenre.length > 0) {
+    // Genre still has related books. Render in same way as for GET route.
+    res.render("genre_delete", {
+      title: "Delete Genre",
+      genre: genre,
+      genre_books: booksInGenre,
+    });
+    return;
+  } else {
+    // Genre has no related books. Delete object and redirect to list of genres.
+    await Genre.findByIdAndDelete(req.body.genreid);
+    res.redirect("/catalog/genres");
+  }
 });
 
 // Display Genre update form on GET.
