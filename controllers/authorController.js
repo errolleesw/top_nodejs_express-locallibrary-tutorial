e542cefaca1f455b02711fbf5adb9bd03ff3e87b
Errol Lee <62831245+errolleesw@users.noreply.github.com>
2024-07-08 23:02:22 +0800

    Added family_name sort back
    Created index on Cosmos DB, added back the family_name sort on the Author Controller.

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -6,7 +6,7 @@ const debug = require("debug")("author");
 
 // Display list of all Authors.
 exports.author_list = asyncHandler(async (req, res, next) => {
-    const allAuthors = await Author.find().exec();
+    const allAuthors = await Author.find().sort({ family_name: 1 }).exec();
     res.render("author_list", {
       title: "Author List",
       author_list: allAuthors,
